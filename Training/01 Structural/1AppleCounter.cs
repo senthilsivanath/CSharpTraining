bbbc994f41a469a30893e175f992894009867036
SenthilSivanath <sssenth@gmail.com>
2015-06-17 11:06:33 +0530

    Adding Solution File

---
+++ Training/01 Structural/1AppleCounter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Training._01_Structural
+{
+    public class AppleCounter
+    {
+        public int CountApples()
+        {
+            int applesGivenByAnoop = 6; // declare and instantiate a variable
+
+            int applesGivenByVimal;  // declaration
+
+            applesGivenByVimal = 10; // intialization
+
+            int totalApples = applesGivenByAnoop + applesGivenByVimal;
+
+            return totalApples;
+        }
+        
+    }
+}
---
+++ Training/01 Structural/2BMICalculator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Training._01_Structural
+{
+    public class BMICalculator
+    {
+        public double calculateBMI(int heighInCentiMeters, int WeightInKg)
+        {
+            int heightInMeter = (heighInCentiMeters / 100);
+
+            double bmi = WeightInKg / (heighInCentiMeters * heighInCentiMeters);
+
+            return bmi;
+        }
+
+
+        public double calculateBMIAlternativwe(int heighInCentiMeters, int WeightInKg)
+        {
+            int heightInMeter = (heighInCentiMeters / 100);
+
+            double bmi = WeightInKg / (Math.Pow(heighInCentiMeters, 2));
+
+            return bmi;
+        }
+    }
+}
